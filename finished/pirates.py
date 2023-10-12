56f56628f526dad7708cce2137ab7567a84deaf4
Galaktro <galaktro@aliensbringchange.com>
2023-10-12 13:41:08 +0200

    auto commit

--- finished/pirates.py
+++
@@ -1,9 +0,0 @@
-class Pirate:
-    def __init__(self, name, role):
-        self.name = name
-        self.role = role
-        
-class Role:
-    def __init__(self, title, rank):
-        self.title = title
-        self.rank = rank
